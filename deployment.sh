36cd074e63b721b4021e4b23af911097f13212e9
Sypth <justsypth@gmail.com>
2024-11-14 15:41:22 +0100

    Patch deployment.sh, made it better

--- deployment.sh
+++ deployment.sh
@@ -11,7 +11,7 @@ CURRENT_DATE=$(date +"%Y-%m-%dT%H:%M")
 jq --arg new_date "$CURRENT_DATE" '.date = $new_date' "$JSON_FILE" > tmp.$$.json && mv tmp.$$.json "$JSON_FILE"
 
 rm -fr /var/www
-mv public /var/www
+cp -r public /var/www
 systemctl restart apache2
 
 echo "Finished deployment. (Hooray :3)"
