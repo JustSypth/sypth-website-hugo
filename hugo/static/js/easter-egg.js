a6b0d9cfccbc44463f272b6083dd9dda4766c645
JustSypth <justsypth@gmail.com>
2025-01-08 15:30:03 +0100

    Change easteregg chance to 1%

--- hugo/static/js/easter-egg.js
+++ hugo/static/js/easter-egg.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', function(event) {
-    const REDIRECT_PROBABILITY = 0.99;
+    const REDIRECT_PROBABILITY = 0.01;
 
     if (event.target.closest('li') && Math.random() < REDIRECT_PROBABILITY) {
         console.log("Easter egg!");
