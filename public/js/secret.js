b9676e9db0a3014e7e511513059f2184ecf3ecc9
JustSypth <justsypth@gmail.com>
2025-01-08 10:40:44 +0100

    Compile

--- public/js/secret.js
+++ public/js/secret.js
@@ -1,9 +1,9 @@
 document.addEventListener('click', function(event) {
     const REDIRECT_PROBABILITY = 0.01;
-    const REDIRECT_URLS = ['/easter-eggs/ml08xo', '/easter-eggs/cma3qb', '/easter-eggs/ti4o7z', '/easter-eggs/mhzt1b'];
 
     if (event.target.closest('li') && Math.random() < REDIRECT_PROBABILITY) {
+        console.log("HIHI");
         let randomIndex = Math.floor(Math.random() * REDIRECT_URLS.length);
-        window.location.href = REDIRECT_URLS[randomIndex];
+        window.location.href = '/easter-egg';
     }
 });
