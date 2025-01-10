1b27d3a3072021e97861ed36c7ea273342483f76
JustSypth <justsypth@gmail.com>
2025-01-10 21:33:12 +0100

    Compile (again)

--- public/public/js/easter-egg.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('click', function(event) {
-    const REDIRECT_PROBABILITY = 0.01;
-
-    if (event.target.closest('li') && Math.random() < REDIRECT_PROBABILITY) {
-        console.log("Easter egg!");
-        window.location.href = '/easter-egg';
-    }
-});
